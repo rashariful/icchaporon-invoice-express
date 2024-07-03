571942dfd43e5ecbd0f66a6b943b2ba9f8340e6f
Source-Code-007 <euhansarkar@gmail.com>
2024-07-03 12:49:30 +0600

    Modified: .YML FIle.

--- src/app/modules/xlToJson/xlToJson.routes.js
+++ src/app/modules/xlToJson/xlToJson.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { upload } from "../../utils/sendImageToCloudinary.js";
-import { XlToJsonController } from "./XlToJson.controller.js";
+import { XlToJsonController } from "./xlToJson.controller.js";
 
 const router = Router();
 router.post("/", upload.single("file"), XlToJsonController.xlToJson);
